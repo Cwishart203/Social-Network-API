90a408020fe00471bd1093261c95ad8ed2409126
Cwishart203 <93050589+Cwishart203@users.noreply.github.com>
2022-01-25 19:51:27 -0500

    Controllers todo's

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -4,26 +4,66 @@ const thoughtController = {
   // get all thoughts
   getThoughts(req, res) {
     // TODO: Your code here
-    
-
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err))
   },
   
   // get single thought by id
   getSingleThought(req, res) {
     // TODO: Your code
-
+    Thought.findOne({_id: req.params.thoughtId})
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({message: "Not found"})
+          : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err))
   },
 
   // create a thought
   createThought(req, res) {
     // TODO: create a thought and add the thought to user's thoughts array
-
+    Thought.create(req.body)
+      .then((thought) => {
+        return User.findOneAndUpdate(
+          {username: req.body.username},
+          {$addToSet: {thought: thought._id}},
+          {new: true}
+        );
+      })
+      .then((user) =>
+      !user
+        ? res
+          .status(404)
+          .json({message: "Not found"})
+        : res.json("Created")
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err)
+      });
   },
 
   // update thought
   updateThought(req, res) {
     // TODO: update thought
-
+    Thought.findOneAndUpdate(
+      {_id: req.params.thoughtId},
+      {$set: req.body},
+      {
+        runValidators: true,
+        new: true
+      }
+    )
+    .then((thought) =>
+    !thought
+      ? res.status(404).json({message: "Not found"})
+      : res.json(thought)
+    )
+    .catch((err) => {
+      res.status(500).json(err)
+    })
   },
 
   // delete thought
@@ -56,13 +96,39 @@ const thoughtController = {
   // add a reaction to a thought
   addReaction(req, res) {
     //  TODO: add reaction to thought's reaction array
-
+    Thought.findOneAndUpdate(
+      {_id: req.params.thoughtId},
+      {$addToSet: {reactions:req.body}},
+      {
+        runValidators: true,
+        new: true
+      }
+    )
+    .then((thought) => {
+      !thought
+        ? res.status(404).json({message: "Not found"})
+        :res.json({message: "Created"})
+    })
+    .catch((err) => res.status(500).json(err))
   },
 
   // remove reaction from a thought
   removeReaction(req, res) {
     // TODO: remove reaction from thoughts
-
+    Thought.findOneAndUpdate(
+      {_id: req.params.thoughtId},
+      {$pull: {reactions: {reactionId: req.params.reactionId}}},
+      {
+        runValidators: true,
+        new: true
+      }
+    )
+    .then((thought) =>
+      !thought
+      ? res.status(404).json({message:"Not found"})
+      : res.json({message:"Deleted"})
+    )
+    .catch((err) => res.status(500).json)
   },
 };
 
